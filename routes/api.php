c37d9eaec3573f3895972ffa7b605a4bc562a9fb
shalvah <diakon.ng@gmail.com>
2021-05-27 12:17:42 +0100

    Add healthcheck endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * Healthcheck
+ *
+ * Check that the service is up. If everything is okay, you'll get a 200 OK response.
+ *
+ * Otherwise, the request will fail with a 400 error, and a response listing the failed services.
+ *
+ * @response 400 scenario="Service is unhealthy" {"status": "down", "services": {"database": "up", "redis": "down"}}
+ * @responseField status The status of this API (`up` or `down`).
+ * @responseField services Map of each downstream service and their status (`up` or `down`).
+ */
+Route::get('/healthcheck', function () {
+    return [
+        'status' => 'up',
+        'services' => [
+            'database' => 'up',
+            'redis' => 'up',
+        ],
+    ];
 });
+
+/*
+Route::middleware('auth:api')->get('/me', function (Request $request) {
+    return $request->user();
+});*/
