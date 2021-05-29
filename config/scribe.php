f56c78a7d6d2e24ac937a2db87e79f57dd5f6e56
shalvah <diakon.ng@gmail.com>
2021-05-29 23:41:49 +0100

    Add Ray

--- config/scribe.php
+++ config/scribe.php
@@ -239,6 +239,9 @@ return [
     'intro_text' => <<<INTRO
 This documentation aims to provide all the information you need to work with our API.
 
+<aside class="success">Example of aside with class=success.</aside>
+<aside class="warning">Example of aside with class=warning.</aside>
+
 <aside>As you scroll, you'll see code examples for working with the API in different programming languages in the dark area to the right (or as part of the content on mobile).
 You can switch the language used with the tabs at the top right (or from the nav menu at the top left on mobile).</aside>
 INTRO
