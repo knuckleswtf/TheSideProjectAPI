7934b0794ff2e5a25ba194a5df93a6d383884591
shalvah <diakon.ng@gmail.com>
2021-06-02 13:18:39 +0100

    Add auth

--- config/scribe.php
+++ config/scribe.php
@@ -194,7 +194,7 @@ return [
         /*
          * Set this to true if any endpoints in your API use authentication.
          */
-        'enabled' => false,
+        'enabled' => true,
 
         /*
          * Set this to true if your API should be authenticated by default. If so, you must also set `enabled` (above) to true.
@@ -230,7 +230,7 @@ return [
          * Any extra authentication-related info for your users. For instance, you can describe how to find or generate their auth credentials.
          * Markdown and HTML are supported.
          */
-        'extra_info' => 'You can retrieve your token by visiting your dashboard and clicking <b>Generate API token</b>.',
+        'extra_info' => 'You can retrieve your token by using the `users/auth` endpoint.',
     ],
 
     /*
