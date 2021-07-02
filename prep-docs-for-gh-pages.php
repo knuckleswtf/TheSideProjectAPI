832b0ecc8615be59122430e22f539a48f974b355
shalvah <diakon.ng@gmail.com>
2021-07-02 14:14:01 +0100

    Update docs

--- prep-docs-for-gh-pages.php
+++ prep-docs-for-gh-pages.php
@@ -1,18 +1,4 @@
 <?php
 
-@mkdir('docs/css', 0777, true);
-@mkdir('docs/js', 0777, true);
-@mkdir('docs/images', 0777, true);
-
-foreach(glob("public/docs/*.*") as $file) {
-    print_r($file, str_replace("public/", "", $file)."\n");
-    copy($file, str_replace("public/", "", $file));
-}
-foreach(glob("public/docs/*/*.*") as $file) {
-    print_r($file, str_replace("public/", "", $file)."\n");
-    copy($file, str_replace("public/", "", $file));
-}
-
-file_put_contents("docs/index.html", str_replace("../docs/", "", file_get_contents("docs/index.html")));
 file_put_contents("docs/CNAME", "demo.scribe.knuckles.wtf");
 echo "Copied docs for GH pages";
